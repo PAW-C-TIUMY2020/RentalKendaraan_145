5db673d6315caaa51a27cdad8eca5b50aac2daae
tendi darisman <50667278+ttendii@users.noreply.github.com>
2020-11-22 20:18:56 +0700

    Add project files.

---
+++ RentalKendaraan_20200140145/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentalKendaraan_20200140145.Models
+{
+    public partial class Customer
+    {
+        public Customer()
+        {
+            Peminjaman = new HashSet<Peminjaman>();
+        }
+
+        public int IdCustomer { get; set; }
+        public string NamaCustomer { get; set; }
+        [RegularExpression("^[0-9]*$", ErrorMessage="Hanya boleh diisi oleh angka!") ]
+        public string Nik { get; set; }
+        public string Alamat { get; set; }
+        public string NoHp { get; set; }
+        public int? IdGender { get; set; }
+
+        public Gender IdGenderNavigation { get; set; }
+        public ICollection<Peminjaman> Peminjaman { get; set; }
+    }
+}
